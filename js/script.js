20d6189e4d9e6a786f433f5fdc89d118912275f3
Nerimene Akari <akari.nerimene@gmail.com>
2018-08-20 15:01:52 +0200

    add initial project

---
+++ js/script.js
@@ -0,0 +1,56 @@
+var myCanvas = document.querySelector(".canvas-container");
+var ctx = myCanvas.getContext("2d");
+var squareWidth = 70;
+var squareHeigth = 35;
+
+// // row 1
+// ctx.fillStyle = "grey";
+// ctx.fillRect(0, 0, squareWidth, squareHeigth);
+
+// ctx.fillStyle = "grey";
+// ctx.fillRect(77, 0, squareWidth, squareHeigth);
+
+// ctx.fillStyle = "grey";
+// ctx.fillRect(154, 0, squareWidth, squareHeigth);
+
+// ctx.fillStyle = "grey";
+// ctx.fillRect(230, 0, squareWidth, squareHeigth);
+
+// row 2
+ctx.fillStyle = "grey";
+ctx.fillRect(0, 38, squareWidth, squareHeigth);
+
+ctx.fillStyle = "grey";
+ctx.fillRect(77, 38, squareWidth, squareHeigth);
+
+ctx.fillStyle = "grey";
+ctx.fillRect(154, 38, squareWidth, squareHeigth);
+
+ctx.fillStyle = "grey";
+ctx.fillRect(230, 38, squareWidth, squareHeigth);
+
+// row 3
+ctx.fillStyle = "grey";
+ctx.fillRect(0, 76, squareWidth, squareHeigth);
+
+ctx.fillStyle = "grey";
+ctx.fillRect(77, 76, squareWidth, squareHeigth);
+
+ctx.fillStyle = "grey";
+ctx.fillRect(154, 76, squareWidth, squareHeigth);
+
+ctx.fillStyle = "grey";
+ctx.fillRect(230, 76, squareWidth, squareHeigth);
+
+// // row 4
+// ctx.fillStyle = "grey";
+// ctx.fillRect(0, 114, squareWidth, squareHeigth);
+
+// ctx.fillStyle = "grey";
+// ctx.fillRect(77, 114, squareWidth, squareHeigth);
+
+// ctx.fillStyle = "grey";
+// ctx.fillRect(154, 114, squareWidth, squareHeigth);
+
+// ctx.fillStyle = "grey";
+// ctx.fillRect(230, 114, squareWidth, squareHeigth);
